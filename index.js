8108f4c4115d5fd7cd30531265d908a618163e9e
Kasin Sergey <bakakasin@gmail.com>
2017-07-14 22:27:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+class Eve {
+  constructor (){
+     this.listeners =  {};
+  }
+ 
+  on(event, handler) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = []
+    }
+    this.listeners[event].push(handler)
+  }
+  off(event) {
+    delete this.listeners[event]
+  }
+  emit(event, ...args) {
+    if (Array.isArray(this.listeners[event])) {
+      this.listeners[eve].forEach(handler => handler(...args))
+    } else {
+      console.log(`Event [ ${event} ] not found`)
+    }
+  }
+}
